1c250336c69dbc9dde573a8f8cf4348d8fbc40b5
kaushalyaanuruth <2023cs089@stu.ucsc.cmb.ac.lk>
2025-08-29 22:54:17 +0530

    added core folder

---
+++ app/core/App.php
@@ -0,0 +1,23 @@
+<?php
+class App{
+
+    private function splitURL(){
+        $URL = $_GET['url'] ?? 'home';
+        return explode('/', $URL);
+        return ($URL);
+    }
+
+    public function loadController(){
+
+        $URL = $this->splitURL();
+
+        $filename = "../app/controllers/" . ucfirst($URL[0]).".php";
+
+        if(file_exists($filename)){
+            require $filename;
+        } else {
+            $filename = "../app/controllers/_404.php";
+            require $filename;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,30 +1,8 @@
 <?php
 
-function show($stuff){
-    echo "<pre>";
-    print_r($stuff);
-    echo "</pre>";
-}
+session_start();
 
+require '../app/core/init.php';
 
-function splitURL(){
-    $URL = $_GET['url'] ?? 'home/index';
-    return explode('/', $URL);
-    return ($URL);
-}
-
-function loadController(){
-
-    $URL = splitURL();
-
-    $filename = "../app/controllers/" . ucfirst($URL[0]).".php";
-    
-    if(file_exists($filename)){
-        require $filename;
-    } else {
-        $filename = "../app/controllers/_404.php";
-        require $filename;
-    }
-}
-
-loadController();
+$app = new App();
+$app->loadController();
